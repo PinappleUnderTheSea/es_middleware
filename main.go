8f31599dc3a6df88d9eddb22e8e3b0287476ece2
ZhuJunjie <zhujunjie.4396@bytedance.com>
2023-08-08 13:24:23 +0800

    fix: handle some errors

--- main.go
+++ main.go
@@ -13,16 +13,22 @@ func main() {
 	models.Init()
 
 	// Register
-	apis.RegisterESMiddleware(apis.ESMiddlewareName, new(apis.Handler))
+	err := apis.RegisterESMiddleware(apis.ESMiddlewareName, new(apis.Handler))
+	if err != nil {
+		log.Err(err)
+		return
+	}
 
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		log.Err(err)
+		return
 	}
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Err(err)
+			return
 		}
 
 		go rpc.ServeConn(conn)
